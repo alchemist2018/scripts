3b2188346e99a6c1f7dc03b45d557883da6cf71d
jim <z.yan16@mail.scut.edu.cn>
2019-11-27 11:57:28 +0800

    initialize

---
+++ sentris/setupRay2alpine.sh
@@ -0,0 +1,15 @@
+#!/bin/sh -e
+apk add --no-cache --virtual .build-deps ca-certificates curl 
+mkdir -m 777 /v2ray 
+cd /v2ray 
+curl -L -H "Cache-Control: no-cache" -o v2ray.zip https://github.com/v2ray/v2ray-core/releases/download/v4.21.3/v2ray-linux-64.zip 
+unzip /v2ray/v2ray.zip 
+rm -rf /v2ray/v2ray.zip 
+rm -rf /v2ray/config.json 
+curl -L -H "Cache-Control: no-cache" -o /v2ray/config.json https://raw.githubusercontent.com/alchemist2018/Ray_PaaS/master/config/server_config.json
+apk del .build-deps
+echo 'nohup /v2ray/v2ray &' > /etc/local.d/v2ray.start
+chmod +x /etc/local.d/v2ray.start
+rc-update add local
+sync
+reboot -f
