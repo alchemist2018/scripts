cf881a3f528d336acabae1ac0bb25d64d367136b
Jim <z.yan16@mail.scut.edu.cn>
2019-12-14 15:02:38 +0800

    update

--- python_scan/cloudflare.py
+++ python_scan/cloudflare.py
@@ -1,17 +1,22 @@
-import urllib
-from urllib.request import urlopen
-
+import requests
+import json
 fo = open('test.txt','r')
 
 for line in fo.readlines():
     line = line.strip()
-    url = 'http://' + str(line) + ':12345/cdn-cgi/trace'
+    url = 'http://' + str(line) + ':443'
     try:
-        y = urlopen(url,timeout=0.2)
-        print(url)
-        fw = open('output.txt','a')
-        fw.write(line + '\n')
-        fw.close()
+        r = requests.get(url, timeout=0.2)
+        content=str(r.headers)
+        content= content.replace("'", '"')
+        data = json.loads(content)
+        if data["Server"] == "cloudflare":
+            print(url)
+            fw = open('output.txt','a')
+            fw.write(url + '\n')
+            fw.close()
+        else:
+            print(line)
     except:
         print(line)
 fo.close()
