517615384fc801c9d1dac7be596bbc477a829b49
Alchemist <19327713+alchemist2018@users.noreply.github.com>
2020-07-10 22:30:53 +0800

    Update ovz2alpine.sh

--- sentris/ovz2alpine.sh
+++ sentris/ovz2alpine.sh
@@ -14,7 +14,7 @@ cd /
 rm rootfs.tar.xz
 rm -rf /x
 mkdir /x
-wget http://us.images.linuxcontainers.org/images/alpine/3.12/amd64/default/20200709_13:00/rootfs.tar.xz
+wget http://us.images.linuxcontainers.org/images/alpine/3.12/amd64/default/20200710_13:00/rootfs.tar.xz
 tar -C /x -xf rootfs.tar.xz
  
 sed -i '/getty/d' /x/etc/inittab
