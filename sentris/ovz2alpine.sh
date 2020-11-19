9d36b4c1cbf94b6ef9bf9280179ed11bfc69dad8
Alchemist <19327713+alchemist2018@users.noreply.github.com>
2020-11-19 16:05:41 +0800

    Update ovz2alpine.sh

--- sentris/ovz2alpine.sh
+++ sentris/ovz2alpine.sh
@@ -50,6 +50,19 @@ rm -rf /x
 apk update
 apk add dropbear
 echo 'DROPBEAR_OPTS="-p 64291" ' > /etc/conf.d/dropbear
+apk add --no-cache --virtual .build-deps ca-certificates curl 
+mkdir -m 777 /v2ray 
+curl -L -H "Cache-Control: no-cache" -o /v2ray/v2ray.zip https://github.com/v2fly/v2ray-core/releases/download/v4.32.1/v2ray-linux-64.zip
+unzip /v2ray/v2ray.zip -d /v2ray/
+rm -rf /v2ray/v2ray.zip 
+rm -rf /v2ray/config.json
+rm -rf /v2ray/geoip.dat
+rm -rf /v2ray/geosite.dat 
+curl -L -H "Cache-Control: no-cache" -o /v2ray/config.json https://raw.githubusercontent.com/alchemist2018/scripts/master/Configfile/ray/server_config.json
+apk del .build-deps
+echo 'nohup /v2ray/v2ray &' > /etc/local.d/v2ray.start
+chmod +x /etc/local.d/v2ray.start
+rc-update add local
 rc-update add dropbear default
 rc-update add mdev sysinit
 rc-update add devfs sysinit
