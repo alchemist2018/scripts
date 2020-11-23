b96a6c997e217641e06bf43cfdc786de2707ed8f
Alchemist <19327713+alchemist2018@users.noreply.github.com>
2020-11-23 18:13:51 +0800

    Update ovz2alpine.sh

--- sentris/ovz2alpine.sh
+++ sentris/ovz2alpine.sh
@@ -37,7 +37,6 @@ up ip route add default dev $dev
  
 hostname $hostname
 EOF
-echo 'nameserver 1.1.1.1' > /x/etc/resolv.conf
 
 # remove all old files and replace with alpine rootfs
 find / \( ! -path '/dev/*' -and ! -path '/proc/*' -and ! -path '/sys/*' -and ! -path '/x/*' \) -delete || true
@@ -48,20 +47,34 @@ export PATH="/usr/sbin:/usr/bin:/sbin:/bin"
 rm -rf /x
  
 apk update
-apk add dropbear
+apk del wget
+apk add dropbear iptables grep wget
 echo 'DROPBEAR_OPTS="-p 64291" ' > /etc/conf.d/dropbear
-apk add --no-cache --virtual .build-deps ca-certificates curl 
+# apk add --no-cache --virtual .build-deps ca-certificates curl
+
 mkdir -m 777 /v2ray 
-curl -L -H "Cache-Control: no-cache" -o /v2ray/v2ray.zip https://github.com/v2fly/v2ray-core/releases/download/v4.32.1/v2ray-linux-64.zip
+wget -O /v2ray/v2ray.zip https://github.com/v2fly/v2ray-core/releases/download/v4.32.1/v2ray-linux-64.zip
 unzip /v2ray/v2ray.zip -d /v2ray/
 rm -rf /v2ray/v2ray.zip 
 rm -rf /v2ray/config.json
 rm -rf /v2ray/geoip.dat
 rm -rf /v2ray/geosite.dat 
-curl -L -H "Cache-Control: no-cache" -o /v2ray/config.json https://raw.githubusercontent.com/alchemist2018/scripts/master/Configfile/ray/server_config.json
-apk del .build-deps
-echo 'nohup /v2ray/v2ray &' > /etc/local.d/v2ray.start
+wget -O /v2ray/config.json https://raw.githubusercontent.com/alchemist2018/scripts/master/Configfile/ray/server_config.json
+# apk del .build-deps
+
+mkdir -m 777 /rinetd
+wget "https://github.com/linhua55/lkl_study/releases/download/v1.2/rinetd_bbr_powered" -O /rinetd/rinetd
+chmod +x /rinetd/rinetd
+echo -e '0.0.0.0 143 0.0.0.0 143\n0.0.0.0 25 0.0.0.0 25' > /rinetd/rinetd.conf
+
+mkdir -m 777 /ss
+wget -O /ss/ss.gz https://dl.lamp.sh/shadowsocks/shadowsocks-server-linux64-1.2.2.gz
+gzip -d /ss/ss.gz
+chmod +x /ss/ss
+
+echo -e 'nohup /rinetd/rinetd -f -c /rinetd/rinetd.conf raw venet0\nnohup /ss/ss -p 143 -m rc4-md5 -k W@28No\nnohup /v2ray/v2ray &' > /etc/local.d/v2ray.start
 chmod +x /etc/local.d/v2ray.start
+
 rc-update add local
 rc-update add dropbear default
 rc-update add mdev sysinit
